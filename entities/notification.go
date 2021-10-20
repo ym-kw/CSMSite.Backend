8a58ef72f6b2caf0726f99f0ad083797a6bddec6
yuta720 <mryy720@yahoo.co.jp>
2021-10-20 22:27:46 +0900

    データ整形

--- entities/notification.go
+++ entities/notification.go
@@ -5,7 +5,6 @@ type Notification struct {
 	Id             int    `column:"id" gorm:"primary_key"`
 	Title          string `column:"title"`
 	Text           string `column:"text"`
-	TargetId       *int   `column:"targetId"`
 	ContuributorId int    `column:"contributorId"`
 	DisableFlag    bool   `column:"disableFlag"`
 	CreatedAt      string `column:"createdAt"`
